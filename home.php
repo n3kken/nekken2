d4b8f60a9d569b3dfe7b94b3a171de7da7e2e226
n3kken <balocofranklinrey@gmail.com>
2026-09-04 20:17:11 +0800

    Title home page

--- home.php
+++ home.php
@@ -12,7 +12,7 @@ $gender     = $_POST["gender"];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Welcome Back</title>
+    <title>Ka galakGalak na pag balik</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
